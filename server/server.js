2067b208098bcd37e218fda49aba6ce4fb28cfcf
Bayron Villanueva <bvillanueva@cognitiva.la>
2018-04-28 19:42:51 -0500

    Creation of POST /todos

---
+++ server/server.js
@@ -0,0 +1,28 @@
+let express = require('express');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(express.json());
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    let todo = new Todo({
+        text : req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        console.log('Error');
+        res.status(400).send(err);
+    })
+
+});
+
+app.listen(3000, () => {
+    console.log('Started at port 3000');
+});
